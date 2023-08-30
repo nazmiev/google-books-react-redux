8a5eaba29bde8ec05e47981eee090665f4534b1c
Роман Назмиев <roma.nazmiev@gmail.com>
2023-08-30 13:05:56 +0700

    skeleton edit

--- src/components/Book/skeleton.tsx
+++ src/components/Book/skeleton.tsx
@@ -2,19 +2,17 @@ import ContentLoader from "react-content-loader"
 
 const Skeleton = () => (
   <ContentLoader 
-    className="book-block"
     speed={2}
-    width={280}
-    height={470}
-    viewBox="0 0 280 500"
+    width={346}
+    height={500}
+    viewBox="0 0 346 500"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
   >
-    <circle cx="140" cy="125" r="123" /> 
-    <rect x="0" y="280" rx="5" ry="5" width="280" height="25" /> 
-    <rect x="0" y="330" rx="5" ry="5" width="280" height="90" /> 
-    <rect x="0" y="450" rx="5" ry="5" width="95" height="30" /> 
-    <rect x="125" y="440" rx="20" ry="20" width="152" height="45" />
+    <rect x="109" y="20" rx="0" ry="0" width="128" height="171" /> 
+    <rect x="109" y="200" rx="0" ry="0" width="128" height="15" /> 
+    <rect x="68" y="230" rx="0" ry="0" width="208" height="29" /> 
+    <rect x="115" y="278" rx="0" ry="0" width="112" height="16" />
   </ContentLoader>
 )
 
